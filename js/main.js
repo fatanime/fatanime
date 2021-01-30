4c5164111b5bcbf3e1b10617c9e525fc24d12068
fatanime <61026865+fatanime@users.noreply.github.com>
2021-01-31 02:49:19 +0400

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -68,6 +68,9 @@
         loop: true,
         animateOut: 'fadeOut',
         animateIn: 'fadeIn',
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:7000,
         items: 1,
         dots: true,
         nav: false,
@@ -189,3 +192,37 @@
 
 
 })(jQuery);
+
+
+/*  gallery */
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            $('.filter').show('1000');
+        }
+        else
+        {
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+
+                if ($(".filter-button").removeClass("active")) {
+            $(this).removeClass("active");
+            }
+                $(this).addClass("active");
+            });
+});
+/*  end gallery */
+
+$(document).ready(function(){
+    $(".fancybox").fancybox({
+        openEffect: "none",
+        closeEffect: "none"
+    });
+});
+   
